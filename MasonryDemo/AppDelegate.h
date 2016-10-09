fc0750fbdacc2f2d6152e52224677db71eaea4e5
王二军 <1695821824@qq.com>
2016-10-09 10:54:27 +0800

    Initial Commit

---
+++ MasonryDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MasonryDemo
+//
+//  Created by 花花 on 16/10/9.
+//  Copyright © 2016年 花花. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
